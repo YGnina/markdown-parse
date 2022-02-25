d1fd7152616c7bffeeb89506a8998c769d981a48
YGnina <93010504+YGnina@users.noreply.github.com>
2022-02-25 00:03:45 -0800

    add stuff for lab4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -53,4 +53,37 @@ public class MarkdownParseTest {
         assertEquals(contents2.size(),0);
     }
 
+/** 
+    @Test
+    public void snippedTest1() throws IOException{
+        List<String> expected_1 = List.of("`google.com","google.com","ucsd.edu");
+        Path fileName = Path.of("Snippet-1.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(expected_1,MarkdownParse.getLinks(contents));
+        
+    }
+    
+
+    @Test
+    public void snippedTest2() throws IOException{
+        List<String> expected_2 = List.of("a.com", "a.com(())", "example.com");
+        Path fileName = Path.of("Snippet-2.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(expected_2,MarkdownParse.getLinks(contents));
+        
+    }
+    */
+
+    @Test
+    public void snippedTest3() throws IOException{
+        List<String> expected_3 = List.of("https://ucsd-cse15l-w22.github.io/");
+        Path fileName = Path.of("Snippet-3.md");
+        String contents = Files.readString(fileName);
+
+        assertEquals(expected_3,MarkdownParse.getLinks(contents));
+        
+    }
+
 }
